b47a9354b86a8de0e029ea19a738f1c9bb7d8455
CristianR10 <cristian_hiei@hotmail.com>
2020-08-25 00:22:48 -0300

    Inicio da pag veiculos, Qa gral HOME

--- application/views/veiculos.php
+++ application/views/veiculos.php
@@ -25,7 +25,7 @@
                         <div class="mm-veiculo__colection">
                             <div class="mm-block-slick">
                                 <div class="mm-block-slick__img">
-                                    <img src="img/img_339x206.png" alt="Teste">
+                                    <img src="/m3motors/style/img/img_339x206.png" alt="Teste">
                                 </div>
                                 <div class="mm-block-slick__block">
                                     <div class="mm-block-slick__title">
@@ -49,11 +49,11 @@
                             <div class="mm-block-slick__btn">
                                 <a href="">Ver mais</a>
                             </div>
-                        </div>                        
+                        </div>                       
                         <div class="mm-veiculo__colection">
                             <div class="mm-block-slick">
                                 <div class="mm-block-slick__img">
-                                    <img src="img/img_339x206.png" alt="Teste">
+                                    <img src="/m3motors/style/img/img_339x206.png" alt="Teste">
                                 </div>
                                 <div class="mm-block-slick__block">
                                     <div class="mm-block-slick__title">
@@ -77,11 +77,11 @@
                             <div class="mm-block-slick__btn">
                                 <a href="">Ver mais</a>
                             </div>
-                        </div>                        
+                        </div>                       
                         <div class="mm-veiculo__colection">
                             <div class="mm-block-slick">
                                 <div class="mm-block-slick__img">
-                                    <img src="img/img_339x206.png" alt="Teste">
+                                    <img src="/m3motors/style/img/img_339x206.png" alt="Teste">
                                 </div>
                                 <div class="mm-block-slick__block">
                                     <div class="mm-block-slick__title">
@@ -105,11 +105,11 @@
                             <div class="mm-block-slick__btn">
                                 <a href="">Ver mais</a>
                             </div>
-                        </div>                        
+                        </div>                       
                         <div class="mm-veiculo__colection">
                             <div class="mm-block-slick">
                                 <div class="mm-block-slick__img">
-                                    <img src="img/img_339x206.png" alt="Teste">
+                                    <img src="/m3motors/style/img/img_339x206.png" alt="Teste">
                                 </div>
                                 <div class="mm-block-slick__block">
                                     <div class="mm-block-slick__title">
@@ -133,11 +133,11 @@
                             <div class="mm-block-slick__btn">
                                 <a href="">Ver mais</a>
                             </div>
-                        </div>                        
+                        </div>                       
                         <div class="mm-veiculo__colection">
                             <div class="mm-block-slick">
                                 <div class="mm-block-slick__img">
-                                    <img src="img/img_339x206.png" alt="Teste">
+                                    <img src="/m3motors/style/img/img_339x206.png" alt="Teste">
                                 </div>
                                 <div class="mm-block-slick__block">
                                     <div class="mm-block-slick__title">
@@ -161,11 +161,11 @@
                             <div class="mm-block-slick__btn">
                                 <a href="">Ver mais</a>
                             </div>
-                        </div>                        
+                        </div>                       
                         <div class="mm-veiculo__colection">
                             <div class="mm-block-slick">
                                 <div class="mm-block-slick__img">
-                                    <img src="img/img_339x206.png" alt="Teste">
+                                    <img src="/m3motors/style/img/img_339x206.png" alt="Teste">
                                 </div>
                                 <div class="mm-block-slick__block">
                                     <div class="mm-block-slick__title">
@@ -189,11 +189,11 @@
                             <div class="mm-block-slick__btn">
                                 <a href="">Ver mais</a>
                             </div>
-                        </div>                        
+                        </div>                       
                         <div class="mm-veiculo__colection">
                             <div class="mm-block-slick">
                                 <div class="mm-block-slick__img">
-                                    <img src="img/img_339x206.png" alt="Teste">
+                                    <img src="/m3motors/style/img/img_339x206.png" alt="Teste">
                                 </div>
                                 <div class="mm-block-slick__block">
                                     <div class="mm-block-slick__title">
@@ -217,11 +217,11 @@
                             <div class="mm-block-slick__btn">
                                 <a href="">Ver mais</a>
                             </div>
-                        </div>                        
+                        </div>                       
                         <div class="mm-veiculo__colection">
                             <div class="mm-block-slick">
                                 <div class="mm-block-slick__img">
-                                    <img src="img/img_339x206.png" alt="Teste">
+                                    <img src="/m3motors/style/img/img_339x206.png" alt="Teste">
                                 </div>
                                 <div class="mm-block-slick__block">
                                     <div class="mm-block-slick__title">
@@ -245,110 +245,14 @@
                             <div class="mm-block-slick__btn">
                                 <a href="">Ver mais</a>
                             </div>
-                        </div>                        
+                        </div>                       
+                                             
                     </div>
                 </div>
             </div>
         </section>
 
-
-        <section>
-            <div class="mm-background">
-                <div class="container">
-                    <div class="row">
-                        <div class="mm-background__block">
-                            <div class="mm-background__form">
-                                <form action="#">
-                                    <label for="" class="name">
-                                        Nome
-                                    </label>
-                                    <input type="text" name="" id="" placeholder="Quem devemos procurar?">
-                                    <label for="" class="email">
-                                        Email
-                                    </label>
-                                    <input type="email" name="" id="" placeholder="nos diga seu melhor email">
-                                    <label for="" class="tel">
-                                        Telefone
-                                    </label>
-                                    <input type="tel" name="" id="" placeholder="Nos diga seu telefone">
-                                    <label for="">Mensagem</label>
-                                    <textarea style="resize: none" name="" id="" cols="30" rows="10"
-                                        placeholder="Fale um pouco mais aqui..."></textarea>
-                                    <div class="mm-background__form-block">
-                                        <button class="mm-background__form-block--btn" type="submit">Enviar
-                                            Mensagem</button>
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="mm-background__content">
-                                <div class="mm-background__line">
-                                </div>
-                                <div class="mm-background__text">
-                                    <h1>
-                                        Diga<br>
-                                        <strong>Olá!</strong>
-                                        <p>Entre em contato para<br>
-                                            mais informações</p>
-                                    </h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section>
-            <div class="mm-social-media">
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-facebook-f"></i>
-                <i class="fab fa-twitter"></i>
-            </div>
-        </section>
+        
     </main>
-
-    <div class="overlay"></div>
-
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="mm-footer">
-                    <div class="col-12 col-md-5">
-                        <ul>
-                            <li><img class="img-fluid" src="img/img-logo-footer.png" alt="logo"></li>
-                            <div class="mm-footer__block">
-                                <li class="mm-footer__block--title">Receber notícias por email</li>
-                                <li class="mm-footer__block--btn">
-                                    <input placeholder="Digite seu email aqui" type="email" name="" id="">
-                                    <button>ok</button>
-                                </li>
-                            </div>
-                        </ul>
-                    </div>
-                    <div class="col-12 col-md-3">
-                        <ul class="mm-footer__block-two">
-                            <li>Artigos</li>
-                            <li>Marcas</li>
-                            <li>Contato</li>
-                            <li>Precisa de ajuda?</li>
-                            <li>Veículos</li>
-                        </ul>
-                    </div>
-                    <div class="col-12 col-md-4">
-                        <ul class="mm-footer__block--info">
-                            <li>Rua para testes 123, Arujá - São Paulo 07425-000</li>
-                            <li>(11) 90000 - 0000</li>
-                            <li>m3motors@m3otors.com.br</li>
-                            <li>Segunda a sexta das 10h00 as 19h00</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="mm-block-end">
-            © 2020 Todos os direitos reservados.
-        </div>
-
-    </footer>
 
 </body>
